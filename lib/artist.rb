489a1b4cc40018a6d2ab6e18f0610bf73a29196f
Carla Holcomb <carla.programmer@gmail.com>
2019-11-09 18:10:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,24 @@
 class Artist
   attr_accessor :name
 
+  @@song_count = 0 
+
   def initialize(name)
     @name = name
   end
 
   def add_song(song)
     song.artist = self
+    @@song_count += 1
   end
 
   def songs
     Song.all.select {|song| song.artist == self}
   end
+
+
+ def self.count
+   @@song_count
+ end
+ 
 end
