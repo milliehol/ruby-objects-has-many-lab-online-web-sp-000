af357523f7ccf24998604c88be70cd9ad863b18d
Carla Holcomb <carla.programmer@gmail.com>
2019-11-09 19:24:07 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,6 +13,16 @@ class Author
     post.author = self
   end
 
+  def add_post_by_title(title)
+   post = Post.new(title)
+   post.author = self
+   @@post_count += 1
+  end
+
+def self.post_count
+   @@post_count
+ end
+
   def posts
     Post.all.select {|post| post.author == self}
   end
